fdd588c27ee73caf6d1def15d58c74373f4a0ca0
Matt Carroll <oholiab@gmail.com>
2015-12-02 17:03:27 +0000

    Does some incorrect sys.argv parsing

--- jailpkg.py
+++ jailpkg.py
@@ -3,7 +3,13 @@ import os
 """Simple scripts for managing packages in jails"""
 
 def main(args):
-    print "stuff"
+    if len(args) < 2:
+        sys.stderr.write("Requires at least 2 args\n")
+        sys.exit(1)
+    name = args[0]
+    jailpath = args[1]
+    command = "pkg install %s -c %s" % (name, jailpath)
+    print command
 
 if __name__ == "__main__":
     main(sys.argv)    
