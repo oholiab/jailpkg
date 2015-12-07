eb35a30ea06d9c051564eec2099200087ef35847
Matt Carroll <mattc@yelp.com>
2015-12-07 11:30:35 +0000

    Got failing test for getting commands from yaml

--- jailpkg/run.py
+++ jailpkg/run.py
@@ -16,12 +16,22 @@ def parse_yaml_packages(filename):
         packagesdict = yaml.load(stream)
     return packagesdict
 
+def yaml_to_commands(configfile):
+    """Parse a yaml file to a list of commands"""
+    commandlist = ()
+    config = parse_yaml_packages(configfile)
+    for jailpath, packages in config.iteritems():
+        for package in packages:
+            commandlist = commandlist.append(generate_command(package, jailpath))
+    return commandlist
+
 def main():
     p = argparse.ArgumentParser(description='Simple script for managing packages in jails')
     p.add_argument('-p', '--package', dest='package', 
             help='name of package to install')
     p.add_argument('-j', '--jailpath', dest='jailpath',
             help='full path to jail to install package in')
+    p.add_argument('-c', '--config', dest='configfile')
     args = p.parse_args()
 
     # Not going to validate these - we'll be passing errors back from pkgng
--- tests/unit/jailpkg_test.py
+++ tests/unit/jailpkg_test.py
@@ -1,9 +1,15 @@
 import pytest
 from jailpkg import run as j
 
+basic_yaml = "tests/unit/fixtures/test_yaml_load_fine_on_hash_load.yaml"
+
 def test_generate_command_expected_output():
     assert j.generate_command('python27', '/some/jail') == 'pkg install python27 -c /some/jail '
 
 def test_yaml_load_fine_on_hash_load():
-    assert j.parse_yaml_packages("tests/unit/fixtures/test_yaml_load_fine_on_hash_load.yaml")\
+    assert j.parse_yaml_packages(basic_yaml)\
             == {"somejail": ["some", "packages"],}
+
+def test_yaml_to_commands_expected_commands():
+    assert j.yaml_to_commands(basic_yaml)\
+            == ("pkg install some -c somejail", "pkg install packages -c somejail")
